1e90bdaa498feea5c4ff4b346a3d3af5624d3932
nam2934 <nam2934@naver.com>
2018-09-12 22:01:30 +0900

    BottomNavigationViewEx library adding(NOT COMPLETED)

--- app/src/main/java/com/example/namjin_jeong/gbt/MainActivity.java
+++ app/src/main/java/com/example/namjin_jeong/gbt/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.namjin_jeong.gbt;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -20,25 +22,36 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_register:
+                    mTextMessage.setText(R.string.title_register);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText(R.string.title_setting);
                     return true;
             }
             return false;
         }
     };
 
+    //@RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        BottomNavigationViewEx bnve = (BottomNavigationViewEx) findViewById(R.id.bnve);
+        bnve.enableShiftingMode(false);
+/*        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        */
     }
 
 }
